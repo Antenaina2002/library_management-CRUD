895c228a9db1f216b90b8d70bb6cbba8f8000c2e
Antenaina2002 <hei.antenaina@gmail.com>
2023-11-19 16:27:37 +0100

    fix: changed the books crud operation to have curdOperations class implemented in it

--- src/booksCRUD.java
+++ src/booksCRUD.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class booksCRUD {
+public class booksCRUD implements crudOperations<booksModel> {
 
     private Connection connection;
     private Statement statement;
@@ -15,7 +15,9 @@ public class booksCRUD {
             throw new RuntimeException(e);
         }
     }
-    public List<booksModel> selectAllBooks() {
+
+    @Override
+    public List<booksModel> findAll() {
         String sql = "SELECT * FROM books;";
         List<booksModel> resultList = new ArrayList<>();
         try {
@@ -36,66 +38,41 @@ public class booksCRUD {
         }
         return resultList;
     }
-    public booksModel selectOneBook(int id) {
-        String sql = "SELECT * FROM books WHERE id = ?;";
-        booksModel resultBook;
-        try {
-            PreparedStatement prepared = connection.prepareStatement(sql);
-            prepared.setInt(1, id);
-            ResultSet result = prepared.executeQuery();
-            result.next();
-            resultBook = new booksModel(
-                    result.getInt("id"),
-                    result.getString("author_name"),
-                    result.getString("book_name"),
-                    result.getInt("page_numbers"),
-                    result.getString("topic"),
-                    result.getDate("release_date"),
-                    result.getBoolean("is_available")
-            );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return resultBook;
+
+    @Override
+    public List<booksModel> saveAll(List<booksModel> toSave) {
+        return null;
     }
-    public void insertBook(booksModel book) {
+
+    @Override
+    public booksModel save(booksModel toSave) {
         String sql = "INSERT INTO books (author_name, book_name, page_numbers, topic, release_date, is_available) VALUES (?, ?, ?, ?, ?, ?);";
         try {
-            PreparedStatement prepared = connection.prepareStatement(sql);
-            prepared.setString(1, book.getAuthorName());
-            prepared.setString(2, book.getBookName());
-            prepared.setInt(3, book.getPageNumbers());
-            prepared.setString(4, book.getTopic());
-            prepared.setDate(5, (Date) book.getReleaseDate());
-            prepared.setBoolean(6, book.isAvailable());
+            PreparedStatement prepared = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            prepared.setString(1, toSave.getAuthorName());
+            prepared.setString(2, toSave.getBookName());
+            prepared.setInt(3, toSave.getPageNumbers());
+            prepared.setString(4, toSave.getTopic());
+            prepared.setDate(5, (Date) toSave.getReleaseDate());
+            prepared.setBoolean(6, toSave.isAvailable());
             prepared.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return toSave;
     }
-    public void updateBook(booksModel book) {
-        String sql = "UPDATE books SET book_name = ?, page_numbers = ?, topic = ?, release_date = ?, is_available = ?, WHERE id = ?;";
-        try {
-            PreparedStatement prepared = connection.prepareStatement(sql);
-            prepared.setString(1, book.getBookName());
-            prepared.setInt(2, book.getPageNumbers());
-            prepared.setString(3, book.getTopic());
-            prepared.setDate(4, (Date) book.getReleaseDate());
-            prepared.setBoolean(5, book.isAvailable());
-            prepared.setInt(7, book.getId());
-            prepared.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public void deleteBook(int id) {
+
+
+    @Override
+    public booksModel delete(booksModel toDelete) {
         String sql = "DELETE FROM books WHERE id = ?;";
         try {
             PreparedStatement prepared = connection.prepareStatement(sql);
-            prepared.setInt(1, id);
+            prepared.setInt(1, toDelete.getId());
             prepared.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return toDelete;
     }
 }
