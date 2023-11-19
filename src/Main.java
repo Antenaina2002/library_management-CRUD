c611fabc545ce51de1fbc8a911d8fe04a1503778
Antenaina2002 <hei.antenaina@gmail.com>
2023-11-19 17:43:42 +0100

    added in main author controller to test the connection.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        authorsCRUD authorsCrud = new authorsCRUD();
+        testAuthorsCrud(authorsCrud);
+    }
+
+    private static void testAuthorsCrud(authorsCRUD authorsCrud) {
+        System.out.println("findAll authors: " + authorsCrud.findAll());
+
+        authorsModel authorToSave = new authorsModel(0, "John Doe", 'M');
+        authorsModel savedAuthor = authorsCrud.save(authorToSave);
+        System.out.println("Saved author: " + savedAuthor);
+
+        List<authorsModel> authorsToSave = Arrays.asList(
+                new authorsModel(0, "Author1", 'F'),
+                new authorsModel(0, "Author2", 'M')
+        );
+        List<authorsModel> savedAuthors = authorsCrud.saveAll(authorsToSave);
+        System.out.println("Saved authors: " + savedAuthors);
+
+        authorsModel authorToDelete = savedAuthors.get(0);
+        authorsModel deletedAuthor = authorsCrud.delete(authorToDelete);
+        System.out.println("Deleted author: " + deletedAuthor);
     }
 }
